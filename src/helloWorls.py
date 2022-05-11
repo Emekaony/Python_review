c65e2f77576a8c560acf49ee19f165624701d629
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-05-11 11:54:39 -0700

    initial commit

---
+++ src/helloWorls.py
@@ -0,0 +1,12 @@
+name = "Nnaemeka"
+
+for i in range(len(name)):
+    print("I have started with python again lol")
+    
+obj = {
+    "name": name,
+    "age": 19,
+    "id": 302255980   
+}
+
+print(obj)
